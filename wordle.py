4ab48f85dc5bbfffde50cdfaebbea872636fff02
C'tri Goudie <ctricoding@ctri.co.uk>
2022-01-27 12:12:11 +0000

    handles yellows, trims cache per execution, uses command line to reset

--- wordle.py
+++ wordle.py
@@ -1,32 +1,81 @@
 import re
-
+import sys
 class Wordle:
     def __init__(self, path) -> None:
         self.path = path
         self.list = self._loadDict(path)
+        self.chars = "" 
+        self.pattern = ""
         print("Loaded {} dictionary entries".format(len(self.list)))
         #self._saveDict(path, self.list)
-        pass
+        
+    def contains(self,chars:str, pattern:str) -> list:
+        if len(chars) == 5 and len(pattern) == 5:
+            self._edge_case_check()    
+            self.contains_green()
+            self.exclude_misses()
+            self.contains_yellow()
+            
+        for i in range(0,min(5,len(self.list))):
+            print(self.list[i].strip())
 
+    def contains_green(self) -> list:
+        chars = self.chars
+        pattern = self.pattern
+        if "G" not in pattern:
+            return
+        re_string = self._extract(chars,pattern,"G")
+        self._trim(re.compile(re_string))
 
-    def contains(self, chars:str, ignore_chars = "") -> list:
-        chars = chars.lower()
-        re_string = ""
-        anything = "[a-z]" if ignore_chars == "" else "[^%s]" % (ignore_chars)
-        for char in chars:
-            re_string += anything if char == "." else char
-        re.compile(re_string)
-        print(re_string)
-        counter = 0
+    def contains_yellow(self):
+        chars = self.chars
+        pattern = self.pattern
+        if "Y" not in pattern:
+            return
+        extracted_chars = self._extract(chars,pattern,"Y",other_char=".")
+        for i in range(0,5):
+            char = extracted_chars[i]
+            if char != ".":
+                
+                re_string = "%s[^%s]%s" % (("."*(i)),char,("."*(4-i)))
+                self._trim(re.compile(re_string))
+                re_string = "(?=.*[%s].*)" % (char)
+                
+                self._trim(re.compile(re_string))
+
+        #(?=.*[uo].*){2}
+
+    def exclude_misses(self):
+        chars = self.chars
+        pattern = self.pattern
+        if "B" not in pattern:
+            return
+        re_string = self._extract(chars,pattern,"B",other_char="")
+        re_string = "[^%s]{5}" % (re_string)
+        self._trim(re.compile(re_string))
+        #[^abou]{5}
+        
+
+    def _extract(self,chars,pattern,patternChar,other_char = "."):
+        return_str = ""
+        if len(chars) == 5 and len(pattern) == 5 and patternChar in pattern: 
+            chars = chars.lower()
+            for i in range(0,5):
+                return_str += chars[i] if pattern[i] == patternChar else other_char
+        else:
+            return ""
+        return return_str
+
+    def _trim(self,regex):
+        print("Current entries [%s], about to trim with: %s" % (len(self.list),regex))
+        newList = []
         for string in self.list:
-            string = string.lower().strip().split(" ")
-            word = string[0]             
-            if re.match(re_string,word):
-                counter += 1
-                print(string)
-            if counter >= 20:
-                break
-
+            newstring = string.lower().strip().split(" ")
+            word = newstring[0]             
+            if re.match(regex,word):
+                #print(string)
+                newList.append(string)
+        self.list = newList
 
 
     def _loadDict(self, path) -> list:
@@ -42,15 +91,18 @@ class Wordle:
             #        data_list.append(word)
         return data_list
 
-    def _saveDict(self, path, list) -> None:
+    def _saveDict(self, path) -> None:
         with open(path, "w+") as file:
-            for line in list:
+            for line in self.list:
                 if line != "":
                     file.write("{}".format(line))
 
 if __name__ == "__main__":
-    w = Wordle(r"wordle_dict.txt")
-    #w.contains(".ro..", "etuiasdfghjklcok")
-    known=input("input your green characters with . as wildcard\n")
-    excluded =  input("input your excluded characters\n")
-    w.contains(known,excluded)
+    print (sys.argv)
+    path = "wordle_dict_base.txt" if "-r" in sys.argv else "wordle_dict.txt"
+    print(path)
+    w = Wordle(path)
+    known=input("input your guesses, leave empty for an opener\n")
+    pattern = input ("input your guesses results as B, Y, or G\n")
+    w.contains(known,pattern)
+    w._saveDict(r"wordle_dict.txt")
